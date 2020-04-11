435d6739e0e64be8a130d40d05c7f012c5a404f9
AwksJen <awksjen@gmail.com>
2020-04-11 18:52:24 -0500

    testing sms post and server

---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+# app.secret_key = os.getenv('SECRET_KEY', 'secretzzz')
+
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_reply():
+    """Respond to incoming calls with a simple text message."""
+    # Start our TwiML response
+    resp = MessagingResponse()
+
+    # Add a message
+    resp.message("The Robots are coming! Head for the hills!")
+
+    return str(resp)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
+    app.run()
